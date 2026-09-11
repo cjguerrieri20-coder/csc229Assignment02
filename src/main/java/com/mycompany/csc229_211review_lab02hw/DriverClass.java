1542fa86faf0b9699d01b3dbc05f76dfac5a8deb
Catherine Guerrieri <guercj1@farmingdale.edu>
2026-09-11 13:44:58 -0400

    Complete Lab 02 review assignment.

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,28 +1,30 @@
 package com.mycompany.csc229_211review_lab02hw;
 
+import java.util.Scanner;
+
 /**
+ * Driver class to instantiate and test the Student class.
  *
- * @author MoaathAlrajab
+ * @author Catherine Guerrieri
  */
 public class DriverClass {
-	
-	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
-	}
 
+	public static void main(String[] args) {
+		// ToDo 5 & ToDo 6: Cast 20 to short to match Student(String, short) constructor
+		Student std1 = new Student("James", (short) 20);
+
+		// ToDo 8: Prompt for GPA, assign it using the setter, and print the student
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter GPA for " + std1.getName() + ": ");
+		double inputGpa = scanner.nextDouble();
+		std1.setGpa(inputGpa);
+
+		// Prints student details via the overridden toString() method in Student.java
+		System.out.println(std1);
+
+		scanner.close();
+
+		// ToDo 9: Inline comments explain functionality; full breakdown provided in submission notes
+		// ToDo 10: Commit, push, and submit via GitHub Pull Request
+	}
 }
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -1,16 +1,55 @@
 package com.mycompany.csc229_211review_lab02hw;
 
 /**
+ * Student class representing a student entity.
+ * Inherits common attributes from Person.
  *
- * @author MoaathAlrajab
+ * @author Catherine Guerrieri
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+// ToDo 1: Make this class a child of Person
+public class Student extends Person {
 
+    // ToDo 3: Add a field for GPA
+    private double gpa;
+
+    // Backing variable for the abstract address methods
+    private String address;
+
+    // ToDo 2 & ToDo 6: Constructors invoking super
+    public Student(String name, short age) {
+        super(name, age);
+    }
+
+    public Student(String name, short age, double gpa) {
+        super(name, age);
+        this.gpa = gpa;
+    }
+
+    // ToDo 2: Implement abstract methods defined in Person
+    @Override
+    public String getAddress() {
+        return this.address;
+    }
+
+    @Override
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    // ToDo 3: Getter and Setter for GPA
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    // ToDo 7: toString method returning formatted Student details
+    @Override
+    public String toString() {
+        return "Student Name: " + getName() +
+                ", Age: " + getAge() +
+                ", GPA: " + gpa;
+    }
 }
